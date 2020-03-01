6dc2bd78d237c00e36ed3ab0fa7bee7794921a34
tjtanjin <cjtanjin@gmail.com>
2020-03-02 03:18:41 +0800

    Moved success message to media caption. Added catch for potential errors.

--- submodules/user_input.py
+++ submodules/user_input.py
@@ -19,16 +19,18 @@ def get_video(update, context):
         update: default telegram arg
         context: default telegram arg
     """
-    chat_id = update.message.chat_id
-    file_id = update.message.video.file_id
-    newFile = context.bot.get_file(file_id, timeout=None)
-    newFile.download('./media/{}.mp4'.format(chat_id))
-    update.message.reply_text("Received file and processing... Please be patient.")
-    mp.video_to_gif(chat_id)
-    context.bot.send_document(chat_id=chat_id, document=open('./media/{}.gif'.format(chat_id), 'rb'))
-    context.bot.send_message(chat_id=chat_id, text="Conversion successful!")
-    os.remove("./media/{}.mp4".format(chat_id))
-    os.remove("./media/{}.gif".format(chat_id))
+    try:
+        chat_id = update.message.chat_id
+        file_id = update.message.video.file_id
+        newFile = context.bot.get_file(file_id, timeout=None)
+        newFile.download('./media/{}.mp4'.format(chat_id))
+        update.message.reply_text("Received file and processing... Please be patient.")
+        mp.video_to_gif(chat_id)
+        context.bot.send_document(chat_id=chat_id, document=open('./media/{}.gif'.format(chat_id), 'rb'), caption="Conversion successful!")
+        os.remove("./media/{}.mp4".format(chat_id))
+        os.remove("./media/{}.gif".format(chat_id))
+    except:
+        context.bot.send_message(chat_id=chat_id, text='An error has occurred. Please open an issue at our <a href="https://github.com/tjtanjin/simple-media-converter">Project Repository</a>!.', parse_mode=ParseMode.HTML, disable_web_page_preview=True)
     return None
 
 def show_help(update, context):
