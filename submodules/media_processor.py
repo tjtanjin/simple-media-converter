973b5b47198b91ca74acfee2210372152d548de3
tjtanjin <cjtanjin@gmail.com>
2020-03-03 00:13:19 +0800

    Significantly reduced file size of gif.

--- submodules/media_processor.py
+++ submodules/media_processor.py
@@ -6,9 +6,11 @@ def video_to_gif(chat_id):
 	Args:
 		chat_id: unique identification for video name
 	"""
+	inputs = {'./media/{}.mp4'.format(chat_id): None}
+	outputs = {'./media/{}.gif'.format(chat_id): '-t 3 -vf "fps=30,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0'}
 	ff = ffmpy.FFmpeg(
-	    inputs={'./media/{}.mp4'.format(chat_id): None},
-	    outputs={'./media/{}.gif'.format(chat_id): None}
+	    inputs=inputs,
+	    outputs=outputs
 	)
 	ff.run()
 	return None
