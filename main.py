3d4761c590b567fd50b002275c698a6b2831514a
tjtanjin <cjtanjin@gmail.com>
2024-01-24 02:02:11 +0800

    chore: Add concurrent_updates to application build

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
 	Handles the initial launch of the program (entry point).
 	"""
 	token = os.getenv("BOT_TOKEN")
-	application = Application.builder().token(token).read_timeout(30).write_timeout(30).build()
+	application = Application.builder().token(token).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
 	load_interactions(application)
 	print("Simple Media Converter instance started!")
 	application.run_polling()
